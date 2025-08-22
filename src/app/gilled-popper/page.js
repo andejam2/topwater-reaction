67f97fdc98bfe39b17b852f2f835850ad2a0e4f6
andejam2 <andejam2@oregonstate.edu>
2025-08-22 11:53:01 -0500

    Added priceId to items list for further testing

--- src/app/gilled-popper/page.js
+++ src/app/gilled-popper/page.js
@@ -2,6 +2,7 @@
 import { useCart } from "../components/cartcontext";
 import { useState } from "react";
 import LureCard from "../components/lurecard";
+import { priceIds } from "../lib/priceIds";
 
 export default function GilledPopperPage() {
     const [quantity, setQuantity] = useState(1);
@@ -11,50 +12,57 @@ export default function GilledPopperPage() {
             id: "gilled-blackwalleye",
             name: "Black with Walleye Skirt",
             price: 12.99,
-            image: "/gilledpopperblack.jpg"
+            image: "/gilledpopperblack.jpg",
+            priceId: priceIds["gilled-blackwalleye"]
         },
         {
             id: "gilled-bluecitrus",
             name: "Blue with Citrus Shad Skirt",
             price: 12.99,
             image: "/gilledpopperblueblack.jpg",
-            priceId: "price_1RyvX2GuGV8LB8ft4w1qP1E3"
+            priceId: priceIds["gilled-bluecitrus"]
         },
         {
             id: "gilled-greenbabybass",
             name: "Green with Baby Bass Skirt",
             price: 12.99,
-            image: "/gilledpoppergreenblack.jpg"
+            image: "/gilledpoppergreenblack.jpg",
+            priceId: priceIds["gilled-greenbabybass"]
         },
         {
             id: "gilled-ivorygoldshiner",
             name: "Ivory with Golden Shiner Skirt",
             price: 12.99,
-            image: "/gilledpopperivoryblack.jpg"
+            image: "/gilledpopperivoryblack.jpg",
+            priceId: priceIds["gilled-ivorygoldshiner"]
         },
         {
             id: "gilled-orangebluegill",
             name: "Orange with Bluegill Skirt",
             price: 12.99,
-            image: "/gilledpopperorangeblack.jpg"
+            image: "/gilledpopperorangeblack.jpg",
+            priceId: priceIds["gilled-orangebluegill"]
         },
         {
             id: "gilled-purplepumpkin",
             name: "Purple with Pumpkin Seed Skirt",
             price: 12.99,
-            image: "/gilledpopperpurpleblack.jpg"
+            image: "/gilledpopperpurpleblack.jpg",
+            priceId: priceIds["gilled-purplepumpkin"]
         },
         {
             id: "gilled-redgizzard",
             name: "Red with Gizzard Shad Skirt",
             price: 12.99,
-            image: "/gilledpopperredblack.jpg"
+            image: "/gilledpopperredblack.jpg",
+            priceId: priceIds["gilled-redgizzard"]
         },
         {
             id: "gilled-yellowsexyshad",
             name: "Yellow with Sexy Shad Skirt",
             price: 12.99,
-            image: "/gilledpopperyellowblack.jpg"
+            image: "/gilledpopperyellowblack.jpg",
+            priceId: priceIds["gilled-yellowsexyshad"]
         },
     ]
     return (
---
+++ src/app/lib/priceIds.js
@@ -0,0 +1,10 @@
+export const priceIds = {
+    "gilled-blackwalleye": "price_1RyxnSGuGV8LB8ftIS70XQ8c",
+    "gilled-bluecitrus": "price_1RyvX2GuGV8LB8ft4w1qP1E3",
+    "gilled-greenbabybass": "price_1RyxtCGuGV8LB8ftb5CHxFW2",
+    "gilled-ivorygoldshiner": "price_1RyxwEGuGV8LB8ftpZBKkApp",
+    "gilled-orangebluegill": "price_1Ryy2YGuGV8LB8ft5jcIEHgU",
+    "gilled-purplepumpkin": "price_1Ryy4EGuGV8LB8ftDMNs18Xy",
+    "gilled-redgizzard": "price_1Ryy5WGuGV8LB8ftn3aC1YYi",
+    "gilled-yellowsexyshad": "price_1Ryy6WGuGV8LB8ft8CxBTa6e",
+};
